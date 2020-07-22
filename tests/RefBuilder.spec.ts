2f1e55fc2b9ba44d56a7d0b1cff50c7aa98c32ef
djonnyx <djonnyx@gmail.com>
2020-07-22 23:37:15 +0300

    refactor: combine-data test renamed to ref-builder

--- tests/RefBuilder.spec.ts
+++ tests/RefBuilder.spec.ts
@@ -2,18 +2,18 @@ import { expect } from 'chai';
 import { TestDataService, NODES_DATA, SELECTORS_DATA, PRODUCTS_DATA, TAGS_DATA, ASSETS_DATA } from "./TestDataService";
 import { RefBuilder } from "../src/RefBuilder";
 
-describe('Data combiner', () => {
-    it('should return hello world', () => {
+describe('RefBuilder', () => {
+    it('should return valid refs', () => {
         const service = new TestDataService();
         const refBuilder = new RefBuilder(service);
         refBuilder.onChange.subscribe(refs => {
-            expect(refs).to.equal({
+            expect(JSON.stringify(refs)).to.equal(JSON.stringify({
                 nodes: NODES_DATA,
                 products: PRODUCTS_DATA,
                 selectors: SELECTORS_DATA,
                 tags: TAGS_DATA,
                 assets: ASSETS_DATA,
-            });
+            }));
         });
     });
 });
